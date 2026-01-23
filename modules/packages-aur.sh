84295ca06ec15996382949120dbbcde31c5add9c
aman <amankumarmatta0@gmail.com>
2026-01-23 15:31:34 +0530

    Modified Hyprlock

--- modules/packages-aur.sh
+++ modules/packages-aur.sh
@@ -3,8 +3,8 @@
 install_aur_packages() {
     local packages=(
         impala tuned hyprsunset tuned-ppd wiremix rofi-wayland bluetui hyprland xdg-desktop-portal-hyprland kitty awww-git hyprlock swaync
-        waybar quickshell fastfetch zsh stow bc jq ffmpeg imagemagick yad hypridle
-        notify-send thunar nwg-look steam neovim openrgb fzf lazygit sddm starship
+        waybar quickshell fastfetch zsh stow bc jq ffmpeg imagemagick yad hypridle everforest-gtk-theme-git
+        notify-send thunar nwg-look steam neovim openrgb fzf lazygit sddm starship treesitter-cli
         sddm-silent-theme zoxide ttf-jetbrains-mono-nerd gamemode lib32-gamemode
         ttf-ibm-plex ttf-roboto noto-fonts spotify spicetify-cli mangohud ttf-gohu-nerd sunset-cursors-git
         zen-browser-bin ttf-twemoji vesktop hyprshot obs-studio obs-vkcapture obs-pipewire-audio-capture swayosd yaru-icon-theme gtk-engine-murrine

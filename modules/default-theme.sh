a99465e4a80729b2960df86252b1e19ed96bb534
zen0x <amanchaitany@proton.me>
2026-02-22 17:05:28 +0530

    updated default-theme.sh

--- modules/default-theme.sh
+++ modules/default-theme.sh
@@ -1,10 +1,13 @@
 apply_default_theme() {
     local THEMES="$HOME/.config/themes"
+    local WAYBAR_THEMES="$HOME/.config/themes-waybar"
     local CURRENT="$THEMES/current"
     local DEFAULT_THEME="Abyssal"
+    local DEFAULT_WAYBAR_THEME="Default"
 
     if [[ -d "$THEMES/$DEFAULT_THEME" ]]; then
         info "Applying default theme: $DEFAULT_THEME"
+        info "Applying default waybar theme: $DEFAULT_WAYBAR_THEME"
 
         ln -sfn "$THEMES/$DEFAULT_THEME" "$CURRENT"
 
@@ -17,5 +20,6 @@ apply_default_theme() {
         ok "Default theme applied."
     else
         warn "Default theme '$DEFAULT_THEME' not found. Skipping theme setup."
+        warn "Default waybar theme '$DEFAULT_WAYBAR_THEME' not found. Skipping waybar theme setup."
     fi
 }

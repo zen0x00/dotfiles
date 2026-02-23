45aacb24b5e9d18ad9a693d7961a5708d601fa52
zen0x <amanchaitany@proton.me>
2026-02-23 19:13:51 +0530

    Added Lazyvim

--- modules/stow.sh
+++ modules/stow.sh
@@ -4,7 +4,7 @@ run_stow() {
     local CONFIG_DIR="$HOME/.config"
     mkdir -p "$CONFIG_DIR"
 
-    local folders=(hypr btop kitty fastfetch rofi gtk-3.0 gtk-4.0 swaync swayosd themes themes-waybar zen0x wlogout)
+    local folders=(hypr btop kitty nvim fastfetch rofi gtk-3.0 gtk-4.0 swaync swayosd themes themes-waybar zen0x wlogout)
 
     for folder in "${folders[@]}"; do
         local target="$CONFIG_DIR/$folder"

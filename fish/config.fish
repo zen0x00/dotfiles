4108778e62cd1e47cdaf1ef2d261cf46ae7b39fe
zen0x <amanchaitany@proton.me>
2026-03-14 10:19:32 +0530

    switched from zsh to fish and cleaned up dir

---
+++ fish/config.fish
@@ -0,0 +1,27 @@
+if status --is-interactive
+    set fish_greeting ""
+    fastfetch
+
+    if type -q starship
+        starship init fish | source
+    end
+
+    fzf --fish | source
+end
+
+
+
+if type -q zoxide
+    zoxide init fish | source
+end
+
+set -x PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.bun/bin
+set -x PATH $PATH $HOME/.spicetify
+set -x PATH $PATH $HOME/.local/share/gem/ruby/3.4.0/bin
+set -x PATH $PATH /home/aman/.dotnet/tools
+
+alias ls='colorls'
+alias vim='nvim'
+alias c='clear'
+alias clock='tty-clock -C 7 -c'
